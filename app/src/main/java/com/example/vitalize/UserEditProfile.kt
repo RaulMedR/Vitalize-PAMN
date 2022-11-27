225cfbaa7f5ac918b3f30a61ea081200c1ee1297
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-11-27 21:00:06 +0000

    SPRINT 2: Templates II - Edición del perfil

--- app/src/main/java/com/example/vitalize/UserEditProfile.kt
+++ app/src/main/java/com/example/vitalize/UserEditProfile.kt
@@ -1,33 +1,33 @@
 package com.example.vitalize
 
-import android.graphics.Bitmap
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.vitalize.data.Resource
-import com.example.vitalize.databinding.FragmentLogInBinding
+import com.example.vitalize.databinding.FragmentUserEditProfileBinding
 import com.example.vitalize.databinding.FragmentUserProfileBinding
 import com.example.vitalize.user.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.NonCancellable.cancel
 
 @AndroidEntryPoint
-class UserProfile : Fragment() {
+class UserEditProfile : Fragment() {
     private val userViewModel by viewModels<UserViewModel>()
     // Binding object instance with access to the views in the game_fragment.xml layout
-    private lateinit var binding: FragmentUserProfileBinding
+    private lateinit var binding: FragmentUserEditProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -35,7 +35,12 @@ class UserProfile : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_user_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_user_edit_profile, container, false)
+        val estaturas = resources.getStringArray(R.array.estaturas)
+        val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, estaturas)
+        Log.d("prueba", binding.autoCompleteTextViewEstaturas.dropDownHeight.toString())
+        binding.autoCompleteTextViewEstaturas.setAdapter(adapter)
+
         return binding.root
     }
 
@@ -43,16 +48,19 @@ class UserProfile : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.userViewModel = userViewModel
         binding.lifecycleOwner = viewLifecycleOwner
-        binding.buttonLogOut.setOnClickListener { logOut() }
-        binding.editUserProfileIcon.setOnClickListener{ toEditUserProfile()}
-        profileDataSync()
+        binding.buttonCancel.setOnClickListener { cancel() }
+        binding.buttonSave.setOnClickListener { guardar() }
+        //profileDataSync()
     }
 
-    private fun toEditUserProfile() {
-        findNavController().navigate(R.id.action_userProfile_to_userEditProfile)
+    private fun guardar() {
+        TODO("Not yet implemented")
+    }
+    private fun cancel(){
+        findNavController().navigate(R.id.action_userEditProfile_to_userProfile)
     }
 
-    private fun profileDataSync() {
+    /*private fun profileDataSync() {
         binding.nombreApellidosProfileUser.text = userViewModel.getNameCurrentUser()
         val photo = userViewModel.getPhotoUrl()
         Log.d("profiledata", photo.toString())
@@ -86,11 +94,8 @@ class UserProfile : Fragment() {
             }
         }
 
-    }
+    }*/
 
-    private fun logOut(){
-        userViewModel.logout()
-        findNavController().navigate(R.id.action_userProfile_to_homeNoSession)
-    }
+
 
 }
