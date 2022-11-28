cd6be3073c16a5f3b62ba7acd44c19bead9e358b
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-11-28 11:59:17 +0000

    SPRINT 2: Templates II - Registro de peso, aparición de calendario como diálogo

--- app/src/main/java/com/example/vitalize/RegisterWeight.kt
+++ app/src/main/java/com/example/vitalize/RegisterWeight.kt
@@ -1,59 +1,77 @@
 package com.example.vitalize
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.vitalize.databinding.FragmentRegisterWeightBinding
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterWeight.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RegisterWeight : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+@AndroidEntryPoint
+class RegisterWeight : Fragment(){
+    // Binding object instance with access to the views in the game_fragment.xml layout
+    private lateinit var binding: FragmentRegisterWeightBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_weight, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register_weight, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterWeight.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterWeight().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.calendarIcon.setOnClickListener { selectDate() }
+        binding.buttonRegistrarPeso.setOnClickListener({registrarPeso()})
+
+        val pesos = resources.getStringArray(R.array.pesos)
+        val adapter_pesos = ArrayAdapter(requireContext(), R.layout.dropdown_item, pesos)
+        binding.autoCompleteTextViewPesos.setAdapter(adapter_pesos)
+
+        val pesos_decimales = resources.getStringArray(R.array.pesos_decimales)
+        val adapter_pesos_decimales = ArrayAdapter(requireContext(), R.layout.dropdown_item, pesos_decimales)
+        binding.autoCompleteTextViewPesosDecimales.setAdapter(adapter_pesos_decimales)
+        //profileDataSync()
     }
+
+    private fun selectDate(){
+        val calendario = Calendar.getInstance()
+        val yy = calendario[Calendar.YEAR]
+        val mm = calendario[Calendar.MONTH]
+        val dd = calendario[Calendar.DAY_OF_MONTH]
+
+
+        val datePicker = DatePickerDialog(
+            requireActivity(),
+            { view, year, monthOfYear, dayOfMonth ->
+                val month = monthOfYear + 1
+                val date = "$dayOfMonth/$month/$year"
+                binding.fecha.setText(date)
+            }, yy, mm, dd
+        )
+
+        datePicker.show()
+    }
+
+
+    private fun registrarPeso(){
+        //findNavController().navigate(R.id.action_userEditProfile_to_userProfile)
+    }
+
+
+
 }
