28f82c07719bdec99fe7ddf37cebb760f5c0ea5c
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-12-04 12:34:41 +0000

    SPRINT 3: Escaneo de productos. Obtención del número del código de barras.

--- app/src/main/java/com/example/vitalize/ScanBarcode.kt
+++ app/src/main/java/com/example/vitalize/ScanBarcode.kt
@@ -1,31 +1,43 @@
 package com.example.vitalize
 
-import android.content.Context
+import android.Manifest
+import android.R
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.Camera
+import android.media.AudioManager
+import android.media.ToneGenerator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.addCallback
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.vitalize.camera.CameraPreview
 import com.example.vitalize.databinding.FragmentScanBarcodeBinding
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.google.zxing.integration.android.IntentIntegrator
+import java.io.IOException
+
 
 class ScanBarcode : Fragment() {
     // Binding object instance with access to the views in the game_fragment.xml layout
     private lateinit var binding: FragmentScanBarcodeBinding
-    private var mCamera: Camera? = null
-    private var mPreview: CameraPreview? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val callback = requireActivity().onBackPressedDispatcher.addCallback(this){
-            findNavController().navigate(R.id.action_scanBarcode_to_homeSession)
+            findNavController().navigate(com.example.vitalize.R.id.action_scanBarcode_to_homeSession)
         }
     }
 
@@ -35,27 +47,30 @@ class ScanBarcode : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, com.example.vitalize.R.layout.fragment_scan_barcode, container, false)
-
-        mCamera = getCameraInstance()
-        mPreview = mCamera?.let {
-            CameraPreview(container!!.context, it)
-        }
-
-        mPreview?.also {
-            val preview: FrameLayout = binding.root.findViewById(R.id.camera_preview)
-            preview.addView(it)
-        }
+        binding.buttonCapture.setOnClickListener{ escanear() }
         return binding.root
     }
 
+    private fun escanear() {
+        val integrator = IntentIntegrator.forSupportFragment(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+        integrator.setPrompt("ESCANEAR CÓDIGO DE BARRAS")
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
 
-    /** A safe way to get an instance of the Camera object. */
-    private fun getCameraInstance(): Camera? {
-        return try {
-            Camera.open() // attempt to get a Camera instance
-        } catch (e: Exception) {
-            // Camera is not available (in use or does not exist)
-            null // returns null if camera is unavailable
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(activity, "Cancelado el escaneo", Toast.LENGTH_LONG).show()
+            } else {
+                binding.barcodeText.setText(result.contents)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
+
+
 }
